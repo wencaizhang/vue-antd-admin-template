2d55fcba98f9b806566d05ced810389d6b3dbfbd
wencaizhang <1052642137@qq.com>
2018-12-24 16:51:50 +0800

    :art: update

---
+++ src/views/Network/IP/Content.vue
@@ -0,0 +1,112 @@
+<template>
+  <div>
+    <div class="table-operator" style="margin-bottom: 16px;">
+      <a-row type="flex" justify="space-between">
+        <a-col>
+          <a-row type="flex" justify="space-between">
+            <a-button icon="sync" @click="handleRefresh" style="margin-right: 10px;" title="刷新"></a-button>
+            <a-button
+              type="primary"
+              style="margin-right: 10px;"
+              icon="plus"
+              @click="handleCreate"
+            >申请IP</a-button>
+            <a-dropdown style="margin-right: 10px;">
+              <a-menu slot="overlay" @click="handleMenuClick">
+                <a-menu-item
+                  v-for="item in menuOptions"
+                  v-if="item.type === 'batch'"
+                  :key="item.id"
+                >{{item.name}}</a-menu-item>
+              </a-menu>
+              <a-button style="margin-left: 8px">批量操作
+                <a-icon type="down"/>
+              </a-button>
+            </a-dropdown>
+            <a-input-search
+              placeholder="input search text"
+              @search="onSearch"
+              style="width: 200px"
+              enterButton
+            />
+          </a-row>
+        </a-col>
+        <a-col>
+          <a-row type="flex" justify="space-between"></a-row>
+        </a-col>
+      </a-row>
+    </div>
+    <a-alert type="info" showIcon style="margin-bottom: 16px;">
+      <div slot="message">
+        已选择&nbsp;
+        <a style="font-weight: 600">{{ selectedRowKeys.length }}</a>&nbsp;&nbsp;项
+        <a style="margin-left: 24px" @click="handleClearSelected">清空</a>
+      </div>
+    </a-alert>
+    <a-table
+      :rowSelection="{selectedRowKeys: selectedRowKeys, onChange: onSelectChange}"
+      :columns="columns"
+      :rowKey="record => record.id"
+      :dataSource="data"
+      :pagination="pagination"
+      :loading="loading"
+      @change="handleTableChange"
+    >
+      <template slot="name" slot-scope="name, record">
+        <a href="javascript:;" @click="handleViewDetail(record)">{{name.first}} {{name.last}}</a>
+      </template>
+      <template slot="operation" slot-scope="text, record">
+        <a-dropdown style="margin-right: 10px;">
+          <a-menu slot="overlay" @click="handleMenuClick($event, record)">
+            <a-menu-item
+              v-for="item in menuOptions"
+              :key="item.id"
+              v-if="item.menu !== false"
+            >{{ item.name }}</a-menu-item>
+          </a-menu>
+          <a-button style="margin-left: 8px">操作
+            <a-icon type="down"/>
+          </a-button>
+        </a-dropdown>
+      </template>
+    </a-table>
+
+    <CreateModal :module="id"/>
+    <BindIP :module="id"/>
+    <FreeIP :module="id"/>
+    <UnbindIP :module="id"/>
+  </div>
+</template>
+
+<script>
+import CreateModal from "./Modal/Create";
+import BindIP from "./Modal/BindIP";
+import FreeIP from "./Modal/FreeIP";
+import UnbindIP from "./Modal/UnbindIP";
+
+import tablePageMixins from "@/utils/mixins/tablePageMixins";
+
+export default {
+  mixins: [tablePageMixins],
+  components: {
+    CreateModal,
+    BindIP,
+    FreeIP,
+    UnbindIP
+  },
+
+  data() {
+    return {
+      module: "network",
+      id: "ip",
+      name: "路由",
+      confirmLoading: false
+    };
+  },
+  computed: {},
+  methods: {}
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Network/IP/Modal/BindIP.vue
@@ -0,0 +1,61 @@
+
+<template>
+  <div>
+    <a-modal
+      @cancel="handleCancel"
+      @ok="handleCreate"
+      :visible="visible"
+      :confirmLoading="confirmLoading"
+      title="绑定公网IP"
+      okText="确定"
+    >
+      <a-form :form="form">
+        <a-form-item :labelCol="{ span: 8 }" :wrapperCol="{ span: 14 }" label="选择一个IP地址：">
+          <a-select
+            placeholder="选择一个IP地址！"
+            v-decorator="[
+              '资源池',
+              {
+                initialValue: 'external_net',
+                rules: [{ required: true, message: '选择一个IP地址！' }]
+              }
+            ]"
+          >
+            <a-select-option value="external_net">external_net</a-select-option>
+            <a-select-option value="external_IPV6">external_IPV6</a-select-option>
+          </a-select>
+        </a-form-item>
+        <a-form-item :labelCol="{ span: 8 }" :wrapperCol="{ span: 14 }" label="选择待绑定端口：">
+          <a-select
+            placeholder="请选择待绑定端口！"
+            v-decorator="[
+              '选择待绑定端口',
+              {
+                initialValue: 'external_net',
+                rules: [{ required: true, message: '请选择待绑定端口！' }]
+              }
+            ]"
+          >
+            <a-select-option value="external_net">external_net</a-select-option>
+            <a-select-option value="external_IPV6">external_IPV6</a-select-option>
+          </a-select>
+        </a-form-item>
+      </a-form>
+    </a-modal>
+  </div>
+</template>
+<script>
+import { baseModalMixins, formModalMixins } from "@/utils/mixins/modalMixin";
+export default {
+  mixins: [baseModalMixins, formModalMixins],
+  data() {
+    return {
+      name: "bind-ip",
+    };
+  },
+
+  methods: {
+
+  }
+};
+</script>
