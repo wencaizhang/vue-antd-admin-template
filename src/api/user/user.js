f1905f3fe96fe2bb7872bd0f9170b03af99819b0
wencaizhang <1052642137@qq.com>
2019-07-16 09:41:11 +0800

    add api

--- src/api/user/user.js
+++ src/api/user/user.js
@@ -10,11 +10,20 @@ export const createUser  = payload => POST('/user/create', payload);
 // 修改密码
 export const modifyPwd   = payload => POST('/user/modifyPwd', payload);
 
+// 修改邮箱
+export const modifyEmail = payload => POST('/user/modifyEmail', payload);
+
+// 修改手机
+export const modifyPhone = payload => POST('/user/modifyPhone', payload);
+
 // 忘记密码
 export const forgotPwd   = payload => POST('/user/forgotPwd', payload);
 
 // 用户基本资料
 export const getBaseInfo = payload => GET('/user/base', payload);
+
+// 更新用户资料
+export const updateUserInfo = payload => POST('/user/update', payload);
 
 // 用户认证
 export const auth        = payload => POST('/user/auth', payload);
@@ -30,3 +39,6 @@ export const getUserList = payload => GET('/user/list', payload);
 
 // 发送验证码
 export const sendCode    = payload => POST('/sms/sendCode', payload);
+
+// 向邮箱发送验证码
+export const sendEmailCode = payload => POST('/email/sendCode', payload);
