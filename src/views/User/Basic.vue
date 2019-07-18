2e67c67154f9ce337bae98879f1603b1f9650598
wencaizhang <1052642137@qq.com>
2019-07-18 10:30:33 +0800

    :art: 修改用户基本信息

--- src/views/User/Basic.vue
+++ src/views/User/Basic.vue
@@ -3,70 +3,71 @@
     <div class="account-settings-info-view">
       <a-row :gutter="16">
         <a-col :md="16" :lg="12">
-          <a-form :form="form">
+          <a-spin :spinning="spinning">
+            <a-form :form="form">
 
-            <a-form-item v-bind="formItemLayout" label="账户类型">
-              {{ userInfo.userType === '1' ? '个人' : '企业' }}
-            </a-form-item>
+              <a-form-item v-bind="formItemLayout" label="账户类型">
+                {{ userInfo.userType === '1' ? '个人' : '企业' }}
+              </a-form-item>
 
-            <a-form-item v-bind="formItemLayout" label="账号名称">
-              {{ userInfo.userName }}
-            </a-form-item>
+              <a-form-item v-bind="formItemLayout" label="账号名称">
+                {{ userInfo.userName }}
+              </a-form-item>
 
-            <a-form-item v-bind="formItemLayout" label="真实姓名">
-              {{ userInfo.realName || '无' }}
-            </a-form-item>
+              <a-form-item v-bind="formItemLayout" label="真实姓名">
+                {{ userInfo.realName || '无' }}
+              </a-form-item>
 
-            <a-form-item v-bind="formItemLayout" label="联系地址">
-              <a-textarea
-                placeholder="请输入联系地址"
-                @change="onChange($event, 'address')"
-                :autosize="{ minRows: 2, maxRows: 6 }"
-                v-decorator="[
-                  'address',
-                  {
-                    initialValue: userInfo.address,
-                    rules:[
-                      { required: true, message: '请输入联系地址' }
-                    ]
-                  }
-                ]"
-              />
-            </a-form-item>
+              <a-form-item v-bind="formItemLayout" label="联系地址">
+                <a-textarea
+                  placeholder="请输入联系地址"
+                  @change="onChange($event, 'address')"
+                  :autosize="{ minRows: 2, maxRows: 6 }"
+                  v-decorator="[
+                    'address',
+                    {
+                      initialValue: userInfo.address,
+                      rules:[
+                        { required: true, message: '请输入联系地址' }
+                      ]
+                    }
+                  ]"
+                />
+              </a-form-item>
 
-            <a-form-item v-bind="formItemLayout" label="联系电话">
-              <a-input
-                @change="onChange($event, 'contactNumber')"
-                placeholder="请输入联系电话"
-                v-decorator="[
-                  'contactNumber',
-                  {
-                    initialValue: userInfo.contactNumber,
-                    rules:[
-                      { required: true, message: '请输入联系电话' }
-                    ]
-                  }
-                ]"
-              />
-            </a-form-item>
-            
-            <a-form-item v-bind="formItemLayout">
-              <a-row>
-                <a-col :offset="8" :span="8">
-                  <a-button
-                    block
-                    :loading="loading"
-                    :disabled="submitAble"
-                    type="primary"
-                    @click="onSubmit"
-                  >
-                    保存
-                  </a-button>
-                </a-col>
-              </a-row>
-            </a-form-item>
-
-          </a-form>
+              <a-form-item v-bind="formItemLayout" label="联系电话">
+                <a-input
+                  @change="onChange($event, 'contactNumber')"
+                  placeholder="请输入联系电话"
+                  v-decorator="[
+                    'contactNumber',
+                    {
+                      initialValue: userInfo.contactNumber,
+                      rules:[
+                        { required: true, message: '请输入联系电话' }
+                      ]
+                    }
+                  ]"
+                />
+              </a-form-item>
+              
+              <a-form-item v-bind="formItemLayout">
+                <a-row>
+                  <a-col :offset="8" :span="8">
+                    <a-button
+                      block
+                      :loading="loading"
+                      :disabled="!submitAble"
+                      type="primary"
+                      @click="onSubmit"
+                    >
+                      保存
+                    </a-button>
+                  </a-col>
+                </a-row>
+              </a-form-item>
+            </a-form>
+          </a-spin>
         </a-col>
       </a-row>
     </div>
@@ -87,8 +88,8 @@ export default {
   data() {
     return {
       userInfo: {},
-      snap: null,
-      submitAble: true,
+      submitAble: false,
+      spinning: false,
 
       form: null,
       loading: false,
@@ -103,16 +104,19 @@ export default {
   },
   methods: {
     async fetch () {
+      this.spinning = true;
       try {
         const resp = await this.$store.dispatch('app/fetchUserInfo')
         this.$store.commit('app/setUserInfo', resp);
         this.userInfo = resp;
       } catch (error) {
         
+      } finally {
+        this.spinning = false;
       }
     },
     onChange (e, field) {
-      this.submitAble = e.target.value == this.userInfo[field];
+      this.submitAble = e.target.value !== this.userInfo[field];
     },
     onSubmit() {
       this.form.validateFieldsAndScroll((err, values) => {
@@ -122,6 +126,8 @@ export default {
             .then(resp => {
               this.userInfo = resp;
               this.loading = false;
+              this.submitAble = false;
+              this.$message.success('保存成功');
             })
             .catch(err => {
               this.loading = false;
