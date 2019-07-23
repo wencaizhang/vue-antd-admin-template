303b02564c36d21fb51d4a6008d1af8b32c0a920
zhangwencai <zhangwencai@urplus.cn>
2019-07-23 17:39:02 +0800

    :wrench: update 部署脚本

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+# npm run build
+
+# 定义变量
+destDir="dist"
+currTime1=`date +%Y-%m-%d`
+currTime2=`date +%H:%M:%S`
+commit=":rocket: deploy @${currTime1} ${currTime2}"
+repo="https://github.com/wencaizhang/vue-admin-template.git"
+localBranch="master"
+remoteBranch="gh-pages"
+# domain="coolfe.fun"
+
+# 进入生成的文件夹
+cd $destDir
+
+# 发布到自定义域名
+# echo $domain > CNAME
+
+# 将前端静态文件初始化为 git 仓库并提交
+# git init
+# git add -A
+# git commit -m "$commit"
+
+git push -f $repo $localBranch:$branch
+
+# 回到上次工作目录
+cd -
